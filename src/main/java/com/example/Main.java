98dc6fef6706877e05f3fd76e83509ec339c3386
Cristinaanaya09 <cris.anaya.ortega@gmail.com>
2024-09-23 18:13:09 +0200

    enhance

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,12 @@
 package com.example;
-import java.util.Date;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Date current = new Date();
-        
-        System.out.println(current);
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Welcome to Madrid" + name + ", come to visit us!");
+        scanner.close();
     }
 }
